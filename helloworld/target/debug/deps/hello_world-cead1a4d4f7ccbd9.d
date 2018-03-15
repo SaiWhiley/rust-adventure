76d1a5a325afcddfe6caa7e4838b6d3bfa1a15c6
SaiWhiley <sai.whiley196@gmail.com>
2018-03-15 15:29:21 +1000

    intro to rust and cargo

---
+++ helloworld/target/debug/deps/hello_world-cead1a4d4f7ccbd9.d
@@ -0,0 +1,5 @@
+C:\Users\saiwh\Dropbox\code\rust\helloworld\target\debug\deps\hello_world-cead1a4d4f7ccbd9.exe: src\main.rs
+
+C:\Users\saiwh\Dropbox\code\rust\helloworld\target\debug\deps\hello_world-cead1a4d4f7ccbd9.d: src\main.rs
+
+src\main.rs:
---
+++ helloworld/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+C:\Users\saiwh\Dropbox\code\rust\helloworld\target\debug\hello_world.exe: C:\Users\saiwh\Dropbox\code\rust\helloworld\src\main.rs
